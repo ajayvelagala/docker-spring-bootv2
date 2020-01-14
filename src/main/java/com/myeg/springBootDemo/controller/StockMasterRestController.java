b0ad022028e425f6576a15a68983b4e86f826696
ajay <velagala.ajay@gmail.com>
2020-01-14 20:34:30 +0800

    changes made to stock controller

--- src/main/java/com/myeg/springBootDemo/controller/StockMasterRestController.java
+++ src/main/java/com/myeg/springBootDemo/controller/StockMasterRestController.java
@@ -11,13 +11,13 @@ public class StockMasterRestController {
 	//adding controller
 	@GetMapping("/addSm")
 	public ResponseEntity<List<StockMasterDto>> addsm(){
-		
+		//adding 
 		List<StockMasterDto> lt = new ArrayList<StockMasterDto>();
 		
 		StockMasterDto st =new StockMasterDto("1", "Ajay", "2000");
 		StockMasterDto st1 =new StockMasterDto("2", "Vijay", "2000");  
 		StockMasterDto st2=new StockMasterDto("3", "Sanjay", "2000");
-		
+		StockMasterDto st3=new StockMasterDto("3", "Sanjay", "2000");
 		lt.add(st);
 		lt.add(st2);
 		lt.add(st1);
