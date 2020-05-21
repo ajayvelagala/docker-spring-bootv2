fe28b7aa7bd0225d12efe6b87ba9ccf3708a287f
ajay <velagala.ajay@gmail.com>
2020-05-21 19:57:29 +0800

    added new users

--- src/main/java/com/myeg/springBootDemo/LoginRestController.java
+++ src/main/java/com/myeg/springBootDemo/LoginRestController.java
@@ -24,12 +24,15 @@ public class LoginRestController {
 		StockMasterDto st3=new StockMasterDto("4", "Veeru", "2000");
 		StockMasterDto st4=new StockMasterDto("5", "user2", "2000");
 		StockMasterDto st5=new StockMasterDto("10", "user10", "2000");
+		StockMasterDto st6=new StockMasterDto("11", "user11", "2000");
 		
 		lt.add(st);
 		lt.add(st2);
 		lt.add(st1);
 		lt.add(st3);
 		lt.add(st4);
+		lt.add(st5);	
+		lt.add(st6);
 	
 		System.out.println("StockMasterDto:"+lt.toString());
 		
